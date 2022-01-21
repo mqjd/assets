8ad39eb5658b670904d2944592d5e9a4ff6a20b6
mqjd <1378415278@qq.com>
2022-01-21 20:19:19 +0800

    add image

--- push.sh
+++ push.sh
@@ -1,3 +1,14 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+imgs=$(find imgs/img -name highcompress*)
+for img in $imgs
+do
+    mv $img ${img/highcompress_/}
+done
+
 git add .
 git commit -m "add image"
 git push
